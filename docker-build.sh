4a878adb2c3dc360775ebe05d48eeccb996470a3
Sébastien <sebastien@birondata.com>
2021-01-18 20:27:48 +0100

    add github action to automatically build docker image

--- docker-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DEFAULT_TAGS="pharoz/domoticz:$(date '+%Y%m%d')"
-echo -n "TAGS [${DEFAULT_TAGS}]:"
-read TAGS
-for TAG in ${TAGS:-${DEFAULT_TAGS}}; do
-    docker build -t "$TAG" .
-done
